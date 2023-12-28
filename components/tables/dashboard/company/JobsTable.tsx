f59f1481574c2e7fa424df8fd7df57c523f2358e
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2023-12-28 16:25:53 +0200

    Company Jobs Table component

--- components/tables/dashboard/company/JobsTable.tsx
+++ components/tables/dashboard/company/JobsTable.tsx
@@ -1,4 +1,5 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Jobs, JobsTableTypes } from "@/types/company";
 import {
   Table,
   TableBody,
@@ -11,80 +12,30 @@ import {
 } from "@/components/ui/table";
 
 import { Badge } from "@/components/ui/badge";
+import { fetchJobsTable } from "@/actions/company";
+import { fetchUser } from "@/actions/user";
+import { formatDate } from "@/utils/dates";
 
-const jobs = [
-  {
-    id: "1",
-    title: "Software Engineer",
-    author: "Company A",
-    date: "2023-12-15",
-    applicants: "23",
-    status: "Open",
-  },
-  {
-    id: "2",
-    title: "Data Scientist",
-    author: "Company B",
-    date: "2023-12-16",
-    applicants: "250",
-    status: "Open",
-  },
-  {
-    id: "3",
-    title: "Product Manager",
-    author: "Company C",
-    date: "2023-12-17",
-    applicants: "1200",
-    status: "Open",
-  },
-  {
-    id: "4",
-    title: "UX/UI Designer",
-    author: "Company D",
-    date: "2023-12-18",
-    applicants: "3",
-    status: "Closed",
-  },
-  {
-    id: "5",
-    title: "Front-end Developer",
-    author: "Company E",
-    date: "2023-12-19",
-    applicants: "15",
-    status: "Open",
-  },
-  {
-    id: "6",
-    title: "Marketing Specialist",
-    author: "Company F",
-    date: "2023-12-20",
-    applicants: "23",
-    status: "Closed",
-  },
-  {
-    id: "7",
-    title: "Financial Analyst",
-    author: "Company G",
-    date: "2023-12-21",
-    applicants: "299",
-    status: "Closed",
-  },
-];
+const JobsTable = async () => {
+  //TODO: INSTEAD OF COMPANY DETAILS ATTACH PUBLISHER / AUTHOR ID TO JOB
+  const { company } = await fetchUser();
+  const { jobs } = await fetchJobsTable();
 
-const JobsTable = () => {
+  // console.log("JOBSTABLE", jobs);
+
   return (
     <Table className="w-full">
       <TableHeader>
         <TableRow>
           <TableHead className="w-[100px] px-0">Job Title</TableHead>
-          <TableHead className="text-center px-0">Posted By</TableHead>
+          <TableHead className="text-center px-0">Managed By</TableHead>
           <TableHead className="text-center px-0">Date</TableHead>
           <TableHead className="text-center px-0">Applicants</TableHead>
           <TableHead className="text-right px-0">Status</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {jobs.map((job) => (
+        {jobs.map((job: any) => (
           <TableRow key={job.id}>
             <TableCell className="font-medium w-4/12 px-0">
               {job.title}
@@ -98,17 +49,17 @@ const JobsTable = () => {
                   />
                   <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
-                <span>Kyriakos V.</span>
+                <span>{company.name}</span>
               </div>
             </TableCell>
             <TableCell className="text-center w-2/12 px-0">
-              {job.date}
+              {formatDate(job.createdAt)}
             </TableCell>
             <TableCell className="text-center w-2/12 px-0">
-              {job.applicants}
+              {job.applicants.length}
             </TableCell>
             <TableCell className="text-right w-1/12 px-0">
-              <Badge variant="outline">{job.status}</Badge>
+              <Badge variant="outline">{job.status.name}</Badge>
             </TableCell>
           </TableRow>
         ))}
