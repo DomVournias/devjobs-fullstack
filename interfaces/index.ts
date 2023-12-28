8a327d0df9f096e6687bf79fd98676f9bfe87ce1
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2023-12-28 16:24:02 +0200

    interfaces

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,21 +1,28 @@
 export interface SelectListItem {
   id: number;
   name: string;
-  min?: number;
-  max?: number;
+  value?: number;
 }
 
-// Location.ts
-export interface Location extends SelectListItem {}
-
-// Industry.ts
-export interface Industry extends SelectListItem {}
-
-// Position.ts
-export interface Position extends SelectListItem {}
-
-// Salary.ts
-export interface Salary extends SelectListItem {
-  min: number;
-  max: number;
+export interface RadioGroupPropTypes {
+  id: number;
+  name: string;
+  value?: number;
 }
+
+// // Location.ts
+// export interface Location extends SelectListItem {}
+
+// // Industry.ts
+// export interface Industry extends SelectListItem {}
+
+// // Position.ts
+// export interface Position extends SelectListItem {}
+
+// // Salary.ts
+// export interface Salary extends SelectListItem {
+//   min: number;
+//   max: number;
+//   minName: string;
+//   maxName: string;
+// }
