b52e15a8a3d7d5321110c8a97e99eab924a55b0d
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2023-12-28 16:23:40 +0200

    Salaries seed

--- prisma/seeds/salaries.js
+++ prisma/seeds/salaries.js
@@ -3,64 +3,74 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const salaries = [
-  { name: "NotSpecified", min: 0, max: 0 },
-  { name: "Lowest", min: 12000, max: 35000 },
-  { name: "EntryLevel", min: 35000, max: 55000 },
-  { name: "Junior", min: 55000, max: 75000 },
-  { name: "MidLevel", min: 75000, max: 90000 },
-  { name: "Senior", min: 90000, max: 120000 },
-  { name: "Lead", min: 65000, max: 75000 },
-  { name: "Director", min: 100000, max: 119999 },
-  { name: "Executive", min: 120000, max: 149999 },
-  { name: "Highest", min: 150000, max: 250000 },
+  { name: "USD 0 per year", value: 0 },
+  { name: "USD 5K per year", value: 5 },
+  { name: "USD 10K per year", value: 10 },
+  { name: "USD 15K per year", value: 15 },
+  { name: "USD 20K per year", value: 20 },
+  { name: "USD 25K per year", value: 25 },
+  { name: "USD 30K per year", value: 30 },
+  { name: "USD 35K per year", value: 35 },
+  { name: "USD 40K per year", value: 40 },
+  { name: "USD 45K per year", value: 45 },
+  { name: "USD 50K per year", value: 50 },
+  { name: "USD 55K per year", value: 55 },
+  { name: "USD 60K per year", value: 60 },
+  { name: "USD 65K per year", value: 65 },
+  { name: "USD 70K per year", value: 70 },
+  { name: "USD 75K per year", value: 75 },
+  { name: "USD 80K per year", value: 80 },
+  { name: "USD 85K per year", value: 85 },
+  { name: "USD 90K per year", value: 90 },
+  { name: "USD 95K per year", value: 95 },
+  { name: "USD 100K per year", value: 100 },
+  { name: "USD 105K per year", value: 105 },
+  { name: "USD 110K per year", value: 110 },
+  { name: "USD 115K per year", value: 115 },
+  { name: "USD 120K per year", value: 120 },
+  { name: "USD 125K per year", value: 125 },
+  { name: "USD 130K per year", value: 130 },
+  { name: "USD 135K per year", value: 135 },
+  { name: "USD 140K per year", value: 140 },
+  { name: "USD 145K per year", value: 145 },
+  { name: "USD 150K per year", value: 150 },
+  { name: "USD 160K per year", value: 160 },
+  { name: "USD 170K per year", value: 170 },
+  { name: "USD 180K per year", value: 180 },
+  { name: "USD 190K per year", value: 190 },
+  { name: "USD 200K per year", value: 200 },
+  { name: "USD 210K per year", value: 210 },
+  { name: "USD 220K per year", value: 220 },
+  { name: "USD 230K per year", value: 230 },
+  { name: "USD 240K per year", value: 240 },
+  { name: "USD 250K per year", value: 250 },
+  { name: "USD 260K per year", value: 260 },
+  { name: "USD 270K per year", value: 270 },
+  { name: "USD 280K per year", value: 280 },
+  { name: "USD 290K per year", value: 290 },
+  { name: "USD 300K per year", value: 300 },
+  { name: "USD 310K per year", value: 310 },
+  { name: "USD 320K per year", value: 320 },
+  { name: "USD 330K per year", value: 330 },
+  { name: "USD 340K per year", value: 340 },
+  { name: "USD 350K per year", value: 350 },
 ];
 
-const load = async () => {
+const seedSalaries = async () => {
+  //! NOTE change the prisma.minSalary to max or min
   try {
-    // Deduplicate the salaries array
-    const uniqueSalaries = [...new Set(salaries)];
-
-    // Check for existing salaries in the database
-    const existingSalaries = await prisma.salary.findMany({
-      where: {
-        name: {
-          in: uniqueSalaries.map((salary) => salary.name),
-        },
-      },
-      select: {
-        name: true,
-      },
-    });
-
-    const existingSalaryNames = existingSalaries.map((p) => p.name);
-
-    // Filter out salaries that already exist
-    const newSalaries = uniqueSalaries
-      .filter((salary) => !existingSalaryNames.includes(salary.name))
-      .map((salary) => ({
-        name: salary.name,
-        min: salary.min,
-        max: salary.max,
-      }));
-
-    console.log("Existing salaries:", existingSalaryNames);
-    console.log("New salaries to be created:", newSalaries);
-
-    if (newSalaries.length > 0) {
-      // Create new salaries
-      await prisma.salary.createMany({
-        data: newSalaries,
+    for (const salary of salaries) {
+      await prisma.maxSalary.create({
+        data: salary,
       });
-      console.log("New salaries created successfully!");
-    } else {
-      console.log("No new salaries to create.");
     }
+
+    console.log("Salaries seeded successfully!");
   } catch (error) {
     console.error(error);
-    process.exit(1);
   } finally {
     await prisma.$disconnect();
   }
 };
 
-load();
+seedSalaries();
