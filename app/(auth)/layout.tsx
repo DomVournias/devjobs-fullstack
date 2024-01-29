18b882c5ab8e5dd90e3750b066c775d69c2f3524
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-01-29 21:09:47 +0200

    Auth - Layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from "react";
 
 import Footer from "@/components/footer/Footer";
-import Header from "@/components/header/Header";
+import Header from "@/components/navigation/headers/Header";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
