b69aaf95147d93ac61e6ef3500e5e5445a226982
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-01-29 21:23:37 +0200

    Dashboard Developer

---
+++ components/dashboards/developer/DeveloperDashboard.tsx
@@ -0,0 +1,11 @@
+import type { Company, Developer } from "@prisma/client";
+
+import React from "react";
+
+export default function DeveloperDashboard() {
+  return (
+    <div>
+      <h2>Developer dashboard</h2>
+    </div>
+  );
+}
